b4cf438c78fcb9fe76a71ab302de96a3b42abd87
helintongh <agh6399@gmail.com>
2023-04-10 19:31:17 +0800

    dpdk hello程序增加注释

---
+++ example-code/helloworld/main.c
@@ -0,0 +1,47 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(c) 2010-2014 Intel Corporation
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include <errno.h>
+#include <sys/queue.h>
+
+#include <rte_memory.h>
+#include <rte_launch.h>
+#include <rte_eal.h>
+#include <rte_per_lcore.h>
+#include <rte_lcore.h>
+#include <rte_debug.h>
+
+static int
+lcore_hello(__attribute__((unused)) void *arg)
+{
+	unsigned lcore_id;
+	lcore_id = rte_lcore_id(); // 5. 返回当前执行单元的线程id
+	printf("hello from core %u\n", lcore_id);
+	return 0;
+}
+
+int
+main(int argc, char **argv)
+{
+	int ret;
+	unsigned lcore_id;
+  // 1. 初始化eal
+	ret = rte_eal_init(argc, argv);
+	if (ret < 0)
+		rte_panic("Cannot init EAL\n");
+
+	/* call lcore_hello() on every slave lcore */
+	RTE_LCORE_FOREACH_SLAVE(lcore_id) { // 2. 遍历除主lcore之外的所有lcore
+		rte_eal_remote_launch(lcore_hello, NULL, lcore_id); // 3. 在core上执行lcore_hello函数
+	}
+
+	/* call it on master lcore too */
+	lcore_hello(NULL);
+
+	rte_eal_mp_wait_lcore(); // 4. 等待lcore完成工作
+	return 0;
+}
