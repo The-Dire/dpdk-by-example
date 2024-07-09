72a34fc7194d27ac6f5972f84688de968e393b4a
helintongh <agh6399@qq.com>
2024-07-09 16:41:31 +0800

    feat:删除分文件

--- src/example/06_netarch_splitfile/ip.h
+++
@@ -1,9 +0,0 @@
-#ifndef __HT_ARP_H__
-#define __HT_ARP_H__
-#include "common.h"
-
-int ht_ip_in(struct rte_mbuf *ip_mbuf, struct rte_mempool *ip_pool);
-int ht_ip_out();
-
-
-#endif
--- src/example/06_netarch_splitfile/udp.h
+++
@@ -1,8 +0,0 @@
-#ifndef __HT_UDP_H__
-#define __HT_UDP_H__
-
-#include "common.h"
-
-void ht_udp_out(struct rte_ipv4_hdr *iphdr, struct rte_mempool *udp_pool);
-
-#endif
